403a761038a13ec0c42e99791287e8a2a6701cfc
jamielill <jl2592c@gre.ac.uk>
2025-09-25 23:41:39 +0100

    added basic template for inventory script

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int money { get; private set; }
+    public int lumber { get; private set; }
+    public int gold { get; private set; }
+    public int copper { get; private set; }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
