3f61064a9386a8574cba8261a4822fd414711c9e
jamielill <jl2592c@gre.ac.uk>
2025-09-25 23:21:11 +0100

    added basic template for real world data script

---
+++ Assets/Scripts/RealWorldData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class RealWorldData : MonoBehaviour
+{
+    public float costLumber { get; private set; }
+    public float costGold { get; private set; }
+    public float costCopper { get; private set; }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    //every 5 mins update prices to reflect real world data
+}
