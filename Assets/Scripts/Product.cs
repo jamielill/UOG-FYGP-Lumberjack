b7a353ff57a36e5d416de702c3fec3e4cfe48d3e
jamielill <jl2592c@gre.ac.uk>
2025-09-25 23:21:29 +0100

    added basic template for Product script

---
+++ Assets/Scripts/Product.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Product : MonoBehaviour
+{
+    /* create variables for each resource required to produce product e.g.
+    public int lumberRequired { get; private set; }
+    public int goldRequired { get; private set; }
+    public int copperRequired { get; private set; }
+    public bool lumberRequiredMet { get; private set; }
+    public bool goldRequiredMet { get; private set; }
+    public bool copperRequiredMet { get; private set; }
+    public int lumberAdded { get; private set; }
+    public int goldAdded { get; private set; }
+    public int copperAdded { get; private set; }
+    
+    */
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    //if requirements met, produce product
+}
